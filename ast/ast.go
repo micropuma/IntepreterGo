a402a0d177aeed32b31e59b7a242307c0a04c8b3
micropuma <micropuma@163.com>
2024-07-05 16:31:34 +0800

    add parser funcitons

--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,14 @@
 package ast
 
-import "monkey/token"
+import ("monkey/token"
+		"bytes"
+	   )
 
 // ============================== 接口定义 ============================== //
 // Node接口必须实现TokenLiteral
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Expression interface {
@@ -36,6 +39,27 @@ type LetStatement struct {
 	Value Expression
 }
 
+type ReturnStatement struct {
+	Token token.Token   // return token
+	ReturnValue Expression
+}
+
+type ExpressionStatement struct {
+	Token token.Token   
+	Expression Expression
+}
+
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+type PrefixExpression struct {
+	Token token.Token      // 记录比如！这种
+	Operator string
+	Right Expression
+}
+
 // fulfill interface
 func (l *LetStatement)statementNode() {}
 func (l *LetStatement)TokenLiteral() string { return l.Token.Literal }
@@ -48,3 +72,79 @@ type Identifier struct {
 // fulfill interface
 func (i *Identifier)expressionNode() {}
 func (i *Identifier)TokenLiteral() string { return i.Token.Literal }
+
+// fulfill interface
+func (rs *ReturnStatement)statementNode() {}
+func (rs *ReturnStatement)TokenLiteral() string { return rs.Token.Literal }
+
+// fulfill interface
+func (es *ExpressionStatement)statementNode() {}
+func (es *ExpressionStatement)TokenLiteral() string { return es.Token.Literal } 
+
+func (p *Program)String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+func (ls *LetStatement)String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (rs *ReturnStatement)String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (es *ExpressionStatement)String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
+func (i *Identifier)String() string {
+	return i.Value
+}
+
+func (il *IntegerLiteral) expressionNode()   {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string { return il.Token.Literal }
+
+func (pl *PrefixExpression) expressionNode()  {}
+func (pl *PrefixExpression) TokenLiteral() string { return pl.Token.Literal }
+func (pl *PrefixExpression) String() string {
+	var out bytes.Buffer
+	
+	out.WriteString("(")
+	out.WriteString(pl.Operator)
+	out.WriteString(pl.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
--- parser/parser.go
+++ parser/parser.go
@@ -4,6 +4,21 @@ import (
 	"monkey/ast"
 	"monkey/lexer"
 	"monkey/token"
+	"fmt"
+	"strconv"
+)
+
+// ===================== 定义parser的优先级 ================
+const (
+	_ int = iota
+
+	LOWEST
+	EQUALS      // ==
+	LESSGREATER // > or <
+	SUM         // +
+	PRODUCT     // *
+	PREFIX      // -X or !X
+	CALL        // myFunction(X	) 
 )
 
 // ====================== 定义parser类 =====================
@@ -12,18 +27,53 @@ type Parser struct {
 
 	curToken token.Token
 	peekToken token.Token
+
+	errors []string
+
+	prefixParseFn map[token.TokenType]prefixParseFn
+	infixParseFn map[token.TokenType]infixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser {
 	// 用lexer做初始化parser
-	p := &Parser{l : l}		
+	p := &Parser{l : l, 
+		         errors : []string{},}		
 
 	// 先读取两个token，已初始化curToken和peekToken
 	p.nextToken()
 	p.nextToken()
 
+	// 注册expression的优先顺序表
+	p.prefixParseFn = make(map[token.TokenType]prefixParseFn)
+	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+	p.registerPrefix(token.BANG, p.parsePrefixExpression)
+	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	return p
 }
+
+func (p *Parser) parseIdentifier() ast.Expression {
+	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+	return lit
+}
+
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn func(ast.Expression) ast.Expression
+)
 
 // ====================================== helper function ==================================
 // 获取下一个token
@@ -46,8 +96,26 @@ func (p *Parser)expectPeek(t token.TokenType) bool{
 		p.nextToken()
 		return true;
 	} else {
+		p.peekError(t)
 		return false;
 	}
+}
+
+func (p *Parser)Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFn[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFn[tokenType] = fn
 }
 
 // =============================================== parse function =======================================
@@ -70,14 +138,39 @@ func (p *Parser)ParseProgram() *ast.Program {
 	return program
 }
 
+func (p *Parser)parseExpression(precedence int) ast.Expression {
+	prefix := p.prefixParseFn[p.curToken.Type]
+	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
+		return nil
+	}
+	leftExp := prefix()
+
+	return leftExp
+}
+
+func (p *Parser)parseExpressionStatement() *ast.ExpressionStatement {
+	stmt := &ast.ExpressionStatement{Token: p.curToken}
+
+	stmt.Expression = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
 // parse Statement ast树结构
 func (p *Parser)parseStatement() ast.Statement {
 	// 判断是否是let statement
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
-		return nil;
+		return p.parseExpressionStatement()
 	}
 }
 
@@ -103,3 +196,32 @@ func (p *Parser)parseLetStatement() *ast.LetStatement {
 	
 	return letStmt
 }
+
+func (p *Parser)parseReturnStatement() *ast.ReturnStatement {
+	returnStmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+	
+	for !p.curTokenIs(token.SEMICOLON) { 
+		p.nextToken()
+	}
+
+	return returnStmt
+}
+
+func (p *Parser) parsePrefixExpression() ast.Expression {
+	expression := &ast.PrefixExpression{
+		Token: p.curToken,
+		Operator: p.curToken.Literal,
+	}
+
+	p.nextToken()
+	expression.Right = p.parseExpression(PREFIX)
+	return expression
+}
+
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
+}
+
